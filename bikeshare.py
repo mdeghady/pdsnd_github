14fba0909d239a2b832d4ea6f7123c418dd2ae9d
mdeghady <mostafadeghady22@gmail.com>
2021-06-26 12:55:01 +0200

    Removing continue from While in get_filters function

--- bikeshare.py
+++ bikeshare.py
@@ -4,7 +4,7 @@ import numpy as np
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
-              'washington': 'washington.csv' }
+              'washington': 'washington.csv' }#dict for the datasets
 
 
 def get_filters():
